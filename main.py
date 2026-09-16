bf97036c0b3df5789fd2d2129c41732bbf9a1763
Arpit-741 <arpitpatel00079@gmail.com>
2026-09-16 09:30:36 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+print(
+    "Hello! Welcome to the E-Comm"
+    "What would you like to do:" \
+    "1. Login" \
+    "2. Product Catalog" \
+    "3. Search Product" \
+    "4. Manage Cart"
+    )
+
+Q= input("Enter Task Name: ")
+match Q:
+    case 1:
+        print("Okay")
+    case _:
+        print("Invalid Request")
+
